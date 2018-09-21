e94846c2d0a0016db461202c5816966cf764b406
benstrac <35542776+benstrac@users.noreply.github.com>
2018-09-21 14:53:26 -0600

    getting there

--- a1.cpp
+++ a1.cpp
@@ -15,35 +15,50 @@ using namespace std;
 
 int main(){
   int errsInserted = 0;
-  char* line;
   queue <char*> data;
   char* pch;
-  ifstream inFile;
-  ofstream outFile;
   
-  inFile.clear();
-  inFile.open(FILENAME);
-  outFile.open("output.html");
+  fstream stream (FILENAME);
+  //  outFile.open("output.html");
   
-  if(inFile.is_open()){
-    //      cout << "open successful\n";
+  if(stream.is_open()){
+      cout << "open successful\n";
 
+      stream.seekg(0, stream.end);
+      int length = stream.tellg();
+      stream.seekg(0, stream.beg);
+
+      char* buffer = new char[length];
+      
+      stream.read(buffer, length);
+      int tagL = strcspn(buffer, "<");
+      //      cout << length << "\n";
+      //      cout << buffer << "\n";
+      char* lines = new char[stream.gcount()];
+      for(int i=0;!stream.eof() && i<=stream.gcount();i++){
+      int tagR = strcspn(buffer, ">");
+	
+      }
+      
+      delete[] buffer;
+      
+      /*
       inFile.getline(line, LENGTH);
       while(line[strlen(line)-1] != '\0'){
-	pch = strtok(line, "<>");
-
+	pch = strtok(line, "<*>");
+	cout << pch << "\n";
 	for(int i=0;pch != NULL;i++){
 	  data.push(pch);
 	  printf("%s\n", data.front());
 	  pch = strtok(NULL, "<>");
 	}
 
-	//	cout << data.front() << '\n';
+	cout << data.front() << '\n';
 	
 	if(strcmp(data.front(), "<h1>")!=0 || strcmp(data.front(), "<h2>")!=0 || strcmp(data.front(), "<p>")!=0){
 	  if(ERRORS != 0){
 	    //get text between tags into line2
-	    line = data.front();
+	    liner = data.front();
 	    //	    cout << line << '\n';
 	    data.pop();
 	  }else
@@ -51,10 +66,11 @@ int main(){
 	}
 	inFile.getline(line, LENGTH);
       }
-      inFile.close();
+      */
+      stream.close();
   }else
     cout << "open unsuccessful\n";  
-
+  
   return 0;
 }
 
